0960aa381f7126c96666f08ce9a41ff94e704e94
twinklinglittlestars <107315845+twinklinglittlestars@users.noreply.github.com>
2022-09-22 20:52:29 -0400

    finishing up code

--- Assets/01-Apple Picker/Prefabs/Basket.cs
+++ Assets/01-Apple Picker/Prefabs/Basket.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // this line enables use of uGUI features
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Find reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        //Get the Text Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();
+        //Set the starting number of points to 0
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -30,8 +39,20 @@ public class Basket : MonoBehaviour
         //Find out what hit this basket
         GameObject collideWith = coll.gameObject;
         if ( collideWith.tag == "Apple" )
-        {
+        { 
             Destroy(collideWith);
+            // Parse the text of the scoreGT into an int
+            int score = int.Parse(scoreGT.text);
+            // add points for catching the apple
+            score += 100;
+            // Convert the score back to a string and display it
+            scoreGT.text = score.ToString();
+
+            //Track the high score
+            if (score > HighScore.score)
+            {
+                HighScore.score = score;
+            }
         }
     }
 }
